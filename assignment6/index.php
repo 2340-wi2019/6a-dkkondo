a5e87d7120ba9e7f505401aa96ef3fb11d7200bb
dkkondo <58612684+dkkondo@users.noreply.github.com>
2020-01-26 13:58:52 -0600

    assignment6
    homework

---
+++ assignment6/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>Assignment 6</title>
+  <meta charset="utf-8"> 
+</head>
+<body>
+	   <!--Dissa Kondo
+     index.html
+     INFO1311.WW
+     Lisa THOENDEL 
+     12/14/19
+-->
+<h1>Hello, world!</h1>
+<div id="result"></div>
+<div>Allow me to introduce myself! My name is <?php echo "Dissa Kondo!"; ?></div>
+<div>I am <?php echo "36"; ?> years old and I am a student at Metropolitan Community College.</div>
+<div>I am currently studying <?php echo "Computer technologie"; ?> and I hope to graduate in <?php echo "2021"; ?></div>
+
+</body>
+</html>
